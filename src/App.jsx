bbc9522a44efdc1784ac15258ff25180779ec111
Muga20 <murimibrian18@outlook.com>
2023-03-09 19:24:34 +0300

    The Begining

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { BrowserRouter, Route, Routes ,Router  } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/style.css'
+import Login from './authentication/Login';
+import Signup from './authentication/Signup';
+import Navbar from './layouts/Navbar';
+import Footer from './layouts/Footer';
+import  './assets/animate.css'
+import './assets/chosen.min.css'
+import  './assets/color-01.css'
+import './assets/font-awesome.min.css'
+import './assets/owl.carousel.min.css'
+import './assets/flexslider.css'
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Shop from './components/Shop';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import About from './components/About';
+
+
+function App() {
+
+  return (
+    <div className="App">
+          <BrowserRouter>
+             <Navbar/>
+          <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path="login" element={<Login/>}/>
+          <Route path="signup"  element={<Signup/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path='shop' element={<Shop/>} />
+          <Route path="cart" element={<Cart/>} />
+          <Route path="details" element={<Details/>}/>
+          <Route path='about'  element={<About/>} />
+          </Routes>
+             <Footer/>
+          </BrowserRouter>  
+    </div>
+  )
+}
+
+export default App
---
+++ src/layouts/Topnav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Topnav() {
+  return (
+    <div>
+    
+    
+    </div>
+  )
+}
+
+export default Topnav
